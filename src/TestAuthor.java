c47f25abcf0f874ffe8ef545567b7df277214977
Bonivárt Krisztián <krisztian.bonivart@gmail.com>
2023-06-12 14:04:05 +0200

    Nem teljes. Printing the name and email of the author from a Book instance. (Hint: aBook.getAuthor().getName(), aBook.getAuthor().getEmail()).
    ezt a reszt meg nem csinaltam meg

--- src/TestAuthor.java
+++ src/TestAuthor.java
@@ -1,2 +1,14 @@
 public class TestAuthor {
+
+    public static void main(String[] args) {
+
+        Author ahTech = new Author("Tan Ah Tech", "ahteck@nowhere.com", 'm');
+        System.out.println(ahTech);
+        ahTech.setEmail("paulTan@nowhere.com");
+        System.out.println("name is: " + ahTech.getName());
+        System.out.println("email is: " + ahTech.getEmail());
+        System.out.println("gender is: " + ahTech.getGender());
+
+
+    }
 }
--- src/TestBook.java
+++ src/TestBook.java
@@ -1,2 +1,27 @@
 public class TestBook {
+
+    public static void main(String[] args) {
+
+
+        Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm');
+        System.out.println(ahTeck);  // Author's toString()
+
+        Book dummyBook = new Book("Java for dummy", ahTeck,19.95,99);
+        System.out.println(dummyBook);  // Test Book's toString()
+        dummyBook.setPrice(29.95);
+        dummyBook.setQty(29);
+
+        System.out.println("name is: " + dummyBook.getName());
+        System.out.println("price is: " + dummyBook.getPrice());
+        System.out.println("qty is: " + dummyBook.getQty());
+        System.out.println("Author is: " + dummyBook.getAuthor());
+        System.out.println("Author name is: " + dummyBook.getAuthor().getName());
+        System.out.println("Author email is: " + dummyBook.getAuthor().getEmail());
+
+        Book anotherBook = new Book("more Java",
+        new Author("Paul Tan", "paul@somewhere.com", 'm'), 29.95);
+        System.out.println(anotherBook);  // toString()
+        System.out.println();
+
+    }
 }
